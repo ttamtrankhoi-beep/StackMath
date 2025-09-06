3024ad583cd1bf864762f2ed50f0d421269bea88
tamtran <ttamtrankhoi@gmail.com>
2025-09-06 20:36:55 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+static bool IsChuoiNgoac(string str)
+{
+    Stack<char> stack = new Stack<char>();
+    Dictionary<char, char> pairC = new Dictionary<char, char>() {
+        {')','(' },
+        {']','[' },
+        {'}','{' }
+    };
+    foreach (char c in str)
+    {
+        if (c == '(' || c == '[' || c == '{')
+        {
+            stack.Push(c);
+        }
+        else if (stack.Count==0 || ((c == ')' || c == ']' || c == '}')))
+        {
+            if (stack.Peek() != pairC[c]) { return false; }
+            stack.Pop();
+        }
+    }
+    return stack.Count == 0;
+}
+string str = "{[}}";
+Console.WriteLine(IsChuoiNgoac(str));
+Console.ReadLine();
